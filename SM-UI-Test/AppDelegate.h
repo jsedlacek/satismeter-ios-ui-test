e0ed472de1c811200cd6ddd447d6967c99ef982f
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2016-09-09 19:56:39 +0200

    Initial Commit

---
+++ SM-UI-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SM-UI-Test
+//
+//  Created by Jakub Sedlacek on 09/09/16.
+//  Copyright © 2016 Jakub Sedlacek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SM-UI-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SM-UI-Test
+//
+//  Created by Jakub Sedlacek on 09/09/16.
+//  Copyright © 2016 Jakub Sedlacek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
